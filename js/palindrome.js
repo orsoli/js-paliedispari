427d7dba438fea7a43117cca48eb57fbf2a03577
orsoli <fljcoli@gmail.com>
2024-08-22 16:09:25 +0200

    add 2-version of palindrome

--- js/palindrome.js
+++ js/palindrome.js
@@ -15,6 +15,34 @@
     3. Call the function
 */
 
+// Version-1
+
+// // --- Preparation Phase
+// // Create variable to save the input word
+// const inputWord = prompt('Insert a word', 'orsro').trim()
+
+// // Create a function to check input word
+// function isPalindrome(inputWord){
+//     // Create a variable to save the reverse word
+//     let reverseWord = "";
+//     // Create a loop to reverse the input word
+//     for(let i = inputWord.length - 1; i >= 0; i--){
+//         // Add letters in reverseWord
+//         reverseWord += inputWord.charAt(i)
+//     }
+//     // Create condition for the result
+//     const result = inputWord === reverseWord ? console.log(`${inputWord} is a Palindrome word`) : console.log(`${inputWord} is not a Palindrome word`)
+//     // Print in console the result
+//     return result;
+// }
+
+// //--- Proccesing phase
+// // Call back function
+// isPalindrome(inputWord)
+
+
+
+// Version-2
 
 // --- Preparation Phase
 // Create variable to save the input word
@@ -23,12 +51,7 @@ const inputWord = prompt('Insert a word', 'orsro').trim()
 // Create a function to check input word
 function isPalindrome(inputWord){
     // Create a variable to save the reverse word
-    let reverseWord = "";
-    // Create a loop to reverse the input word
-    for(let i = inputWord.length - 1; i >= 0; i--){
-        // Add letters in reverseWord
-        reverseWord += inputWord[i]
-    }
+    const reverseWord = inputWord.split('').reverse().join('')
     // Create condition for the result
     const result = inputWord === reverseWord ? console.log(`${inputWord} is a Palindrome word`) : console.log(`${inputWord} is not a Palindrome word`)
     // Print in console the result
